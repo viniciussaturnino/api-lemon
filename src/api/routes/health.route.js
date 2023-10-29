14ac717d5cf6452e97db836de3e035a22a1bc596
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-29 10:24:40 -0300

    refactor healthcheck route

--- src/api/routes/health.route.js
+++ src/api/routes/health.route.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/health-check", (request, response) => response.status(200).json({
-  message: "OK"
-}));
+router.get("/health-check", async (_request, response, _next) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now()
+  };
+  try {
+      response.status(200).send(healthcheck);
+  } catch (error) {
+      healthcheck.message = error;
+      response.status(503).send();
+  }
+});
 
 module.exports = router;

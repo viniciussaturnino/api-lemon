dc394f3760077947a259365991481b57b14cfce2
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-29 12:10:12 -0300

    add initial structure to services

---
+++ src/api/services/cost.service.js
@@ -0,0 +1,10 @@
+const { customerRules } = require("../../config/vars")
+
+
+class CostService {
+  getCostBasedOnHistory(consumptionHistory) {
+    // Do some math here
+  }
+}
+
+module.exports = CostService;
---
+++ src/api/services/customer.service.js
@@ -0,0 +1,13 @@
+const { customerRules } = require("../../config/vars")
+
+
+class CustomerService {
+  validateConsumptionClass(consumptionClass) {
+  }
+
+  validateFeeModality(feeModality) {
+  }
+
+}
+
+module.exports = CustomerService;

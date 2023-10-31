8a86b87154d4bc7d3d5124774a8f2c6d2115ec50
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-30 21:48:21 -0300

    feat: create customer controller

---
+++ src/api/controllers/customer.controller.js
@@ -0,0 +1,44 @@
+const { cpf, cnpj } = require("cpf-cnpj-validator")
+
+const { validate } = require("../services/customer.service");
+const { schemaErrors } = require("../errors/api.errors")
+
+
+exports.validate = async (request, response, next) => {
+  const {
+    numeroDoDocumento: documentNumber,
+    tipoDeConexao: conectionType,
+    classeDeConsumo: consumptionClass,
+    modalidadeTarifaria: feeModality,
+    historicoDeConsumo: consumptionHistory,
+  } = request.body;
+
+  if (documentNumber.length === 11 && !cpf.isValid(documentNumber)) {
+    return response.status(400).json({
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        schemaErrors.INVALID_CPF
+      ]
+    });
+  }
+
+  if (documentNumber.length === 14 && !cnpj.isValid(documentNumber)) {
+    return response.status(400).json({
+      elegivel: false,
+      razoesDeInelegibilidade: [
+        schemaErrors.INVALID_CNPJ
+      ]
+    });
+  }
+
+  const responseBody = validate(
+    conectionType,
+    consumptionClass,
+    feeModality,
+    consumptionHistory
+  );
+
+  const responseStatus = (responseBody.elegivel === true) ? 200 : 400;
+
+  return response.status(responseStatus).json(responseBody);
+};

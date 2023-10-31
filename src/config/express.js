c76c610e5a8d35ff8c046fb82941d3915bcea66e
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-30 21:39:04 -0300

    refactor: express config

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const { ValidationError } = require("express-validation");
+
 const routes = require("../api/routes");
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(routes);
+
+app.use(function(error, _request, response, _next) {
+  if (error instanceof ValidationError) {
+    return response.status(error.statusCode).json(error)
+  }
+  return response.status(500).json(error)
+})
 
 module.exports = app;

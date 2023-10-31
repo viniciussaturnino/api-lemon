2cf00e86db3884c0659b93dd02f5a984cabc6726
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-30 21:39:52 -0300

    fix: customer schema

--- src/api/schemas/customer.schema.js
+++ src/api/schemas/customer.schema.js
@@ -10,23 +10,25 @@ const tiposDeConexao = Object.values(tiposDeConexaoEnum);
 const classesDeConsumo = Object.values(classesDeConsumoEnum);
 const modalidadesTarifarias = Object.values(modalidadesTarifariasEnum);
 
-const customerSchema = Joi.object({
-  numeroDoDocumento: Joi.string()
-    .pattern('^\\d{11}$'|'^\\d{14}$')
-    .required(),
-  tipoDeConexao: Joi.string()
-    .valid(...tiposDeConexao)
-    .required(),
-  classeDeConsumo: Joi.string()
-    .valid(...classesDeConsumo)
-    .required(),
-  modalidadeTarifaria: Joi.string()
-    .valid(...modalidadesTarifarias)
-    .required(),
-  historicoDeConsumo: Joi.array()
-    .items(Joi.number().integer())
-    .min(3)
-    .max(12)
-});
+const customerSchema = {
+  body: Joi.object({
+    numeroDoDocumento: Joi.string()
+      .pattern(/(^\d{11}$)|(^\d{14}$)/)
+      .required(),
+    tipoDeConexao: Joi.string()
+      .valid(...tiposDeConexao)
+      .required(),
+    classeDeConsumo: Joi.string()
+      .valid(...classesDeConsumo)
+      .required(),
+    modalidadeTarifaria: Joi.string()
+      .valid(...modalidadesTarifarias)
+      .required(),
+    historicoDeConsumo: Joi.array()
+      .items(Joi.number().integer())
+      .min(3)
+      .max(12)
+  })
+};
 
 module.exports = customerSchema;

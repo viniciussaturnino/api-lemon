d42f7cfd6306bf5ba0b9af1dfafd94661c9eee43
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-30 21:47:18 -0300

    feat: create energy service

---
+++ src/api/services/energy.service.js
@@ -0,0 +1,10 @@
+const { customerRules } = require("../../config/vars");
+
+exports.getEconomyBasedOnHistory = (consumptionHistory) => {
+  const validMonths = consumptionHistory.slice(0, 12);
+  const total = validMonths.reduce((a, b) => a + b, 0);
+  const average = total / validMonths.length;
+  const CO2Economy = (customerRules.CO2Emission * (average * 12)) / customerRules.KWh;
+
+  return { average, CO2Economy };
+};

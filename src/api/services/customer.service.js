46ae6d9e78aea461357391f8afb0c8a348e9b55f
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-10-30 21:47:45 -0300

    feat: create customer service

--- src/api/services/customer.service.js
+++ src/api/services/customer.service.js
@@ -1,13 +1,53 @@
-const { customerRules } = require("../../config/vars")
+const { validConsumptionClasses, validFeeModality, validMinCustomerConsumption } = require("../schemas/validation")
+const { getEconomyBasedOnHistory: getCostBasedOnHistory } = require("./energy.service");
+const { customerErrors } = require("../errors/api.errors")
 
 
-class CustomerService {
-  validateConsumptionClass(consumptionClass) {
+function _validateConsumptionClass(consumptionClass) {
+  if (!validConsumptionClasses.includes(consumptionClass)) {
+    return customerErrors.INVALID_CLASS;
   }
-
-  validateFeeModality(feeModality) {
-  }
-
+  return;
 }
 
-module.exports = CustomerService;
+function _validateFeeModality(feeModality) {
+  if (!validFeeModality.includes(feeModality)) {
+    return customerErrors.INVALID_FEE_MODALITY;
+  }
+  return;
+}
+
+function _validateMinConsumption(average, conectionType) {
+  if (average < validMinCustomerConsumption[conectionType]) {
+    return customerErrors.LOW_CONSUMPTION;
+  }
+  return;
+}
+
+exports.validate = (
+  conectionType,
+  consumptionClass,
+  feeModality,
+  consumptionHistory
+  ) => {
+  const { average, CO2Economy } = getCostBasedOnHistory(consumptionHistory);
+
+  const errors = [
+    _validateConsumptionClass(consumptionClass),
+    _validateFeeModality(feeModality),
+    _validateMinConsumption(average, conectionType),
+  ].map((error) => error).filter((error) => error);
+
+  const isValid = (errors.length === 0) ? true : false;
+  const responseBody = {
+    elegivel: isValid
+  };
+
+  if (isValid) {
+    responseBody.economiaAnualDeCO2 = CO2Economy;
+  } else {
+    responseBody.razoesDeInelegibilidade = errors;
+  }
+
+  return responseBody;
+}

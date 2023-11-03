507d0bca4576a23aed078fa2332f93ac16f9bb4f
viniciussaturnino <viniciussaturnino78@gmail.com>
2023-11-03 10:34:15 -0300

    doc: Implement swagger docs

--- src/config/express.js
+++ src/config/express.js
@@ -1,11 +1,15 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUI = require('swagger-ui-express');
 const { errorHandler } = require('../api/errors/error.handler');
+const swaggerConfig = require('./swagger.json');
 
 const routes = require('../api/routes');
 
 const app = express();
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerConfig));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
